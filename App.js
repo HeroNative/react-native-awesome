70dd4f1b0ea99d8f72b24990182a45a08c227c2d
Mickey <mickey@Mickeys-iMac.local>
2018-01-09 03:36:40 +1000

    justifycontent and alignitems

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ class Blink extends Component{
 export default class FixedDimensionsBasics extends Component{
   render() {
     return (
-      <View style={{flex:1, flexDirection:'row'}}>
+      <View style={{flex:1, flexDirection:'column', justifyContent: 'space-between', alignItems:'center'}}>
         <View style={{width:50, height:50, backgroundColor:'powderblue'}}/>
         <View style={{width:50, height:50, backgroundColor:'skyblue'}}/>
         <View style={{width:50, height:50, backgroundColor:'steelblue'}}/>
