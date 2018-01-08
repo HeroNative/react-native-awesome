70d3c48f88273364b384e501e1c3273bb01623d4
Mickey <mickey@Mickeys-iMac.local>
2018-01-09 01:33:27 +1000

    FixedDimensionsBasics

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import BlinkApp from './App';
-import LotsOfStyles from './App';
+import FixedDimensionsBasics from './App';
 
-AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
